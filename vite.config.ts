c8d51f5a47b3b791549300828ba7907ffc67c4e1
Rafael Ribeiro <ralkribeiro@hotmail.com>
2026-05-03 18:34:48 -0300

    fix vercel
    Signed-off-by: Rafael Ribeiro <ralkribeiro@hotmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,25 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
+    base: "./", // 🔥 ESSENCIAL
+
+    server: {
+        host: "::",
+        port: 8080,
+        hmr: {
+            overlay: false,
+        },
     },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+    plugins: [
+        react(),
+        mode === "development" && componentTagger()
+    ].filter(Boolean),
+
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 }));
